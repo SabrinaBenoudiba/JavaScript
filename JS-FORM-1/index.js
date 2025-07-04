2165dbdbeeed7583c28b7cc274323b6d21edb998
SabrinaBenoudiba <sabrina.benoudiba@gmail.com>
2025-07-04 08:07:13 +0200

    maj dossier JS

--- JS-FORM-1/index.js
+++ JS-FORM-1/index.js
@@ -104,13 +104,91 @@
 // };
 
 
-function filtrer(category){
-  let produits = document.querySelectorAll("#products li");
-  produits.forEach(produit => {
-    if(category === "all" || produit.classList.contains(category)){
-      produit.classList.remove("hide");
-    }else{
-      produit.classList.add("hide");
-    }
-  });
-}
+
+//initialilisation d'une fonction avec paramètre catégorie
+// function filtrer(category){ 
+// //tu selectionnes tous les éléments article dans la section article (ou li si ça aurait été une liste) products.
+//   let produits = document.querySelectorAll("#products article"); 
+// //je parcours chaque produit dans la liste produits
+//   produits.forEach(produit => {
+// //si categroy est strictement = à all ou si un produit possède ou contient la class de la catégory sélenctioné (de ma fonction créé filtrage de mes category)
+//     if(category === "all" || produit.classList.contains(category)){
+// //on affiche le produit (on enlève la classe hide)
+//       produit.classList.remove("hide");
+//     }else{
+// // on enlève le produit (on ajoute la classe hide)
+//       produit.classList.add("hide");
+//     }
+//   });
+// }
+
+// document.getElementById('mon-bouton').onclick = function(event) {
+//   console.log(event);
+// };
+
+//Changer la couleur du parent au clic en jaune
+// let boutons = document.getElementsByTagName("button");
+// for (let i = 0; i< boutons.length; i++){
+//   boutons[i].onclick = function (event){
+//     event.target.parentNode.style.backgroundColor ="yellow";
+//   }
+// }
+
+//color au click et enlève la couleur au click
+// let boutons = document.getElementsByTagName("button");
+// for (let i = 0; i< boutons.length; i++){
+//   boutons[i].onclick = function (event){
+//     let parent = event.target.parentNode ;
+//     if (parent.style.backgroundColor === "yellow"){
+//       parent.style.backgroundColor ="";
+//     }else{
+//       parent.style.backgroundColor ="yellow";
+//     }
+//   }
+// }
+
+//Changer la couleur du parent et grand parent au click et les remettre comme avant au click
+// let boutons = document.getElementsByTagName("button");
+// for (let i = 0; i< boutons.length; i++){
+//   boutons[i].onclick = function (event){ 
+//     let parentDiv = event.target.parentNode ;
+//     let grandParentDiv = parentDiv.parentNode ; //le parent du parent
+//   if (parentDiv.style.backgroundColor === "yellow" && grandParentDiv.style.backgroundColor === "blue"){
+//     parentDiv.style.backgroundColor = "";
+//     grandParentDiv.style.background = "";
+//   }else{
+//     parentDiv.style.backgroundColor ="yellow" //je rappelle ma variable parentDiv créé plus haut
+//     grandParentDiv.style.backgroundColor ="blue" //je rappelle ma variable grandParentDiv créé plus haut
+//   }
+//   }
+// };
+
+//***************************Exo 11.2 *******************************//
+// Ajouter puis supprimer un élément html depuis JS (nous n'allons certainement pas l'uliser mais il faut savoir le faire)
+// 1. créer l'élément
+// var bouton = document.createElement('button');
+// // 2. créer le contenu du bouton (noeud textuel)
+// var texteDuBouton = document.createTextNode('Mon beau bouton créé via JS');
+// // 3. ajouter le contenu au bouton
+// bouton.appendChild(texteDuBouton);
+// // 4. ajouter le bouton au body de la page
+// document.body.appendChild(bouton);
+// // 5. supprimer ce qu'on vient de créer 
+// bouton.parentNode.removeChild(bouton);
+
+
+// création de mes variables input et button
+var saisieChamp = document.createElement('input');
+var bouton = document.createElement('button');
+// Ajout du contenu (du texte) à l'intérieur du bouton (j'ai utilisé ma variable bouton)
+bouton.appendChild(document.createTextNode('clique ici pour effacer ta saisie dans le champ'));
+//Ajout de la saisie du champ et le bouton dans la section id exo 11-2 que j'ai créé dans mon html
+document.getElementById("exo11-2").appendChild(saisieChamp);
+document.getElementById("exo11-2").appendChild(bouton);
+// Création d'une fonction pour que lorsque l'on clique dans la saisie du champ la valeur soit vide
+onclick = function (){
+  saisieChamp.value ='';
+};
+
+
+
